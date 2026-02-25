cb30d072681e5edc31f8c46401c1b42e86852896
Molnar-Adam <molnar.a2003@gmail.com>
2026-02-25 10:42:08 +0100

    Hibák javítása

--- main.cpp
+++ main.cpp
@@ -5,33 +5,35 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     //NELEMENTS helyett N_ELEMENTS
-    int *b = new int[NELEMENTS];
+    int *b = new int[N_ELEMENTS];
 
     //Aposztróf helyett dupla idézőjel kell és hiányzik a "<< std::endl;"
-    std::cout << '1-100 ertekek dup lazasa'
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
 
     //Hiányzik a feltétel és az "i++"
-    for (int i = 0;)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
     //Hibás feltétel
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         //Hiányzik a "b[i]" érték és a "<< std::endl;"
-        std::cout << "Ertek:"
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     //Az 'atlag' változó nincs inicializálva és célszerű lenne lebegőbontos változót használni átlaghoz 
-    int atlag;
+    double atlag = 0;
     //Vessző van pontosvessző helyett
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         //Hiányzik a ";"
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     //Hiányzik a memória felszabadítás;
+    delete[] b;
+    
     return 0;
 }
