d85e632e85d9c89fe7df9c22bfee7d7b4930084b
Molnar-Adam <molnar.a2003@gmail.com>
2026-02-25 10:38:09 +0100

    Hibák kommentezése

--- main.cpp
+++ main.cpp
@@ -4,23 +4,34 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+    //NELEMENTS helyett N_ELEMENTS
     int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
+
+    //Aposztróf helyett dupla idézőjel kell és hiányzik a "<< std::endl;"
+    std::cout << '1-100 ertekek dup lazasa'
+
+    //Hiányzik a feltétel és az "i++"
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
+    //Hibás feltétel
     for (int i = 0; i; i++)
     {
+        //Hiányzik a "b[i]" érték és a "<< std::endl;"
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
+    //Az 'atlag' változó nincs inicializálva és célszerű lenne lebegőbontos változót használni átlaghoz 
     int atlag;
+    //Vessző van pontosvessző helyett
     for (int i = 0; i < N_ELEMENTS, i++)
     {
+        //Hiányzik a ";"
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    //Hiányzik a memória felszabadítás;
     return 0;
 }
